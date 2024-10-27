5a2ed8bd6ae3843e760ea251c3f0460711215722
7levis <levy.7mx@gmail.com>
2024-10-27 19:06:27 -0300

    inicio menu

---
+++ GestorDeBiblioteca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GestorDeBiblioteca
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("\nSistema de Biblioteca");
+            Console.WriteLine("1. Adicionar Livro.");
+            Console.WriteLine("2. Listar Livros.");
+            Console.WriteLine("3. Emprestar Livro.");
+            Console.WriteLine("4. Devolver Livro.");
+            Console.WriteLine("5. Sair.");
+
+        }
+    }
+
+} 
